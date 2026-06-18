8c703c987c5b8c757f93f3742ff0de2284ecced1
Lvshujun0918 <lvshujun0918@163.com>
2026-06-18 12:28:49 +0800

    feat(vite): 适配调试

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8282',
-        changeOrigin: true
-      }
+export default defineConfig(({ command }) => {
+    // 开发环境下通过代理访问时使用子路径
+    const isDev = command === 'serve'
+    return {
+        plugins: [vue()],
+        base: isDev ? '/absproxy/3000/' : '/',  // 生产环境可根据需要调整
+        server: {
+            allowedHosts: ['code.cszj.wang'],
+            port: 3000,
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:8282',
+                    changeOrigin: true
+                }
+            }
+        },
+        build: {
+            outDir: 'dist',
+            assetsDir: 'assets'
+        }
     }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
 })
